c7d6fc576de35666c1e6a9137f2988d69116ffb8
Kevin Buffardi <kbuffardi@csuchico.edu>
2018-09-27 15:34:52 -0700

    Add sentence input

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Kevin Buffardi and class
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string said;  
+
+  cout<<"What do you say?\n";
+  getline(cin,said);
+
+  cout<<"As a shout, you said:\n"<<said<<endl;
+
+  return 0;
+}
