fb6908d1324ac502a3a9b56dec47264a561d1e11
xar8 <jevans42@ecc-jaguar.csuchico.edu>
2018-09-28 10:46:12 -0700

    Final Draft

--- main.cpp
+++ main.cpp
@@ -7,11 +7,29 @@ using namespace std;
 int main()
 {
   string said;  
+  int len;
+  char letters;
 
   cout<<"What do you say?\n";
   getline(cin,said);
+  len = said.length();
+  
+  cout<<"As a shout, you said:\n";
 
-  cout<<"As a shout, you said:\n"<<said<<endl;
-
+  for(int index=0;index<len;index++)
+    {
+      if (said[index] > 90) 
+        {        
+          letters = said[index]-32;
+          cout<<letters;
+        }
+      else
+        {
+          letters = said[index];
+          cout<<letters;
+        }
+    }
+  cout<<endl;
+  
   return 0;
 }
